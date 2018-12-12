dae3648c8188eeb58521ae104eb44e7de78d3042
jaysaligia <773294157@qq.com>
2018-12-12 16:42:56 +0800

    add the database

--- app/src/main/java/daixiahu/totem/Act1.java
+++ app/src/main/java/daixiahu/totem/Act1.java
@@ -1,12 +1,10 @@
 package daixiahu.totem;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import java.sql.BatchUpdateException;
+
 //this is poker
 public class Act1 extends AppCompatActivity {
 
