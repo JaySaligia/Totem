38d451d3d390f3850ad6095da415d85e55660eab
jaysaligia <773294157@qq.com>
2018-12-12 15:08:03 +0800

    Initial commit

---
+++ app/src/main/java/daixiahu/totem/Act1.java
@@ -0,0 +1,27 @@
+package daixiahu.totem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import java.sql.BatchUpdateException;
+
+public class Act1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act1_layout);
+        Button button1 = (Button) findViewById(R.id.Act1_Button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+
+    }
+}
