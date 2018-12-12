794a099dd19b3db04a0e1b189f25d8db1cb377c3
jaysaligia <773294157@qq.com>
2018-12-12 15:34:40 +0800

    test

--- app/src/main/java/daixiahu/totem/Act1.java
+++ app/src/main/java/daixiahu/totem/Act1.java
@@ -18,7 +18,7 @@ public class Act1 extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-
+            //test
             }
         });
 
