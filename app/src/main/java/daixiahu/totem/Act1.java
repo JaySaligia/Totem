641a945093e825cc5815a4cd111fad9f52fe2a03
jaysaligia <773294157@qq.com>
2019-01-11 11:36:45 +0800

    进展：正在写磁盘io

--- app/src/main/java/daixiahu/totem/Act1.java
+++ app/src/main/java/daixiahu/totem/Act1.java
@@ -1,12 +1,15 @@
 package daixiahu.totem;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-//this is poker
-//hahaha
+import daixiahu.totem.backstage.DatabaseManager;
+
 public class Act1 extends AppCompatActivity {
 
     @Override
@@ -14,13 +17,19 @@ public class Act1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act1_layout);
         Button button1 = (Button) findViewById(R.id.Act1_Button1);
-        button1.setOnClickListener(new View.OnClickListener(){
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-            //test
+            public void onClick(View v) {
+                newdatabase("Schools");
+                newdatabase("Companies");
             }
         });
-
-
     }
+        public int newdatabase(String databasename){
+            DatabaseManager db = new DatabaseManager();
+            db.newdatabase(this, databasename);
+            return 1;
+        }
+
+
 }
